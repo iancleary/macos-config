f5580e699351a21def9bd51a7e405c0c973901a8
iancleary <github@iancleary.me>
2024-04-22 17:21:58 -0700

    Reorganize configuration

--- flake.nix
+++ flake.nix
@@ -25,53 +25,6 @@
     }:
   let
     forAllSystems = nixpkgs.lib.genAttrs flake-utils.lib.defaultSystems;
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-
-      imports = [
-        # Include the results of the hardware scan.
-        # nixvim.nixDarwinModules.nixvim
-        ./modules/allow-unfree.nix
-        ./modules/nix-direnv.nix
-      ];
-
-      environment.systemPackages = with pkgs;
-        [ 
-          just
-          graphite-cli
-
-          # VS Code (for extensions)
-          nodejs_18
-
-          # For terminal output with chezmoi dotfiles...merging over to this config
-          colorls
-
-          # while migrating dotfiles
-          chezmoi
-        ];
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
     overlays = {
         unstable = final: prev: {
           unstable = nixpkgs-unstable.legacyPackages.${prev.system};
@@ -93,8 +46,8 @@
     darwinConfigurations."macbookAir" = nix-darwin.lib.darwinSystem {
       pkgs = legacyPackages."aarch64-darwin";
       system = "aarch64-darwin";
-      modules = [ 
-        configuration
+      modules = [
+        ./modules/nix-darwin/default.nix
         home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -103,6 +56,7 @@
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
+        
         ./home-manager/default.nix # myHome definition
       ];
       specialArgs = { inherit inputs; };
--- modules/nix-direnv.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, ... }: 
-
-{
-  # https://github.com/nix-community/nix-direnv
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
-}
