9db6c25e24f90f83fec1766f527af4c0d89024a6
iancleary <github@iancleary.me>
2024-04-22 17:23:32 -0700

    Update formatting and remove ;

--- modules/nix-darwin/default.nix
+++ modules/nix-darwin/default.nix
@@ -1,47 +1,49 @@
-{ pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
+{ pkgs, ... }: 
 
-      # Allow unfree packages
-      nixpkgs.config.allowUnfree = true;
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
 
-      # https://github.com/nix-community/nix-direnv
-      programs.direnv.enable = true;
-      programs.direnv.nix-direnv.enable = true;
-        
-      environment.systemPackages = with pkgs;
-        [ 
-          just
-          graphite-cli
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-          # VS Code (for extensions)
-          nodejs_18
+  # https://github.com/nix-community/nix-direnv
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+    
+  environment.systemPackages = with pkgs;
+    [ 
+      just
+      graphite-cli
 
-          # For terminal output with chezmoi dotfiles...merging over to this config
-          colorls
+      # VS Code (for extensions)
+      nodejs_18
 
-          # while migrating dotfiles
-          chezmoi
-        ];
+      # For terminal output with chezmoi dotfiles...merging over to this config
+      colorls
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+      # while migrating dotfiles
+      chezmoi
+    ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-      # programs.fish.enable = true;
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 4;
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+};
